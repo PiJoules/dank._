f042d21b5a84bd6a9b1b433beeacfc9b57c97d4b
pijoules <lchan1994@yahoo.com>
2015-10-17 17:56:45 -0400

    gotta increase that github score

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+# Compile an unexpected .cena into a .dank
+dankify () {
+	pro=$1
+	if [[ $1 == *.cena ]]
+	then
+		player_name=${pro::-5}
+		casual=$player_name.c # Casual c
+		outplaid=$player_name.dank # Output
+		args=${@:2} # All arguments starting with the third one
+		cp $pro $casual
+
+		gcc $casual -lm -o $player_name.dank
+		./$outplaid $args
+
+		rm $casual
+		return 0
+	else
+		echo "You scrub, a .cena was expected."
+		return 1
+	fi
+}
